2705f75275a9580af39124fbf2af634a326a1463
Ajay More 7 <the7ajaymore@gmail.com>
2025-03-31 17:28:14 +0530

    Initial Commit

---
+++ ClaudeDoList/ClaudeDoListApp.swift
@@ -0,0 +1,17 @@
+//
+//  ClaudeDoListApp.swift
+//  ClaudeDoList
+//
+//  Created by Ajay More on 31/03/25.
+//
+
+import SwiftUI
+
+@main
+struct ClaudeDoListApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ ClaudeDoListTests/ClaudeDoListTests.swift
@@ -0,0 +1,17 @@
+//
+//  ClaudeDoListTests.swift
+//  ClaudeDoListTests
+//
+//  Created by Ajay More on 31/03/25.
+//
+
+import Testing
+@testable import ClaudeDoList
+
+struct ClaudeDoListTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
